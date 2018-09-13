757767efa7a2d811f5bfe96cdaefd13f0495ac2c
NICOLAS SIMMONDS SAMPER <n.simmonds10@sis.virtual.uniandes.edu.co>
2018-09-13 15:31:34 -0500

    Listo buffer, creo

--- Caso1-infraComp/src/novasoft/Buffer.java
+++ Caso1-infraComp/src/novasoft/Buffer.java
@@ -3,20 +3,20 @@ package novasoft;
 import java.util.ArrayList;
 
 public class Buffer {
-	
-	private int max = 10;
+
+	public final static int max = 10;
 	private ArrayList<Mensaje> mensajes = new ArrayList<Mensaje>();
 	private ArrayList<Cliente> clientesA = new ArrayList<Cliente>();
 	private int nMensajes;
-	
+
 	private static Buffer buffer;
-	
+
 	private Buffer() {
 		mensajes = new ArrayList<Mensaje>();
 		clientesA = new ArrayList<Cliente>();
 		nMensajes = 0;
 	}	
-	
+
 	public static Buffer getBuffer() {
 		if(buffer == null) {
 			buffer = new Buffer();
@@ -37,24 +37,25 @@ public class Buffer {
 			}
 			adentro=true;
 			notifyAll();
-			
+
 		}
 		return adentro;
 	}
-	
-	
-	
-	
-	public void retirar(Servidor ser){
-		while(true){
-			if(mensajes.size() > 0){
-				mensajes.remove(mensajes.size()-1);
-				this.notifyAll();
-			}
-			else{
-				ser.yield();
-			}
+
+
+
+
+	public synchronized Mensaje retirar(Servidor ser)
+	{
+		Mensaje mens=null;
+		if(mensajes.size() > 0)
+		{
+			 mens=mensajes.get(mensajes.size()-1);
+			mensajes.remove(mensajes.size()-1);
+			nMensajes--;
+			this.notifyAll();
 		}
+	return mens;
 	}
 
 }
