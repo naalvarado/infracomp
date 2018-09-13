59626600fab37cc6f8f7c24dcc6956eba9e02f1b
Nicolas <n.simmonds10@uniandes.edu.co>
2018-09-12 21:06:44 -0500

    cambios buffer

--- Caso1-infraComp/src/novasoft/Buffer.java
+++ Caso1-infraComp/src/novasoft/Buffer.java
@@ -7,25 +7,43 @@ public class Buffer {
 	private int max = 10;
 	private ArrayList<Mensaje> mensajes = new ArrayList<Mensaje>();
 	private ArrayList<Cliente> clientesA = new ArrayList<Cliente>();
+	private int nMensajes;
 	
-	public void meter(Mensaje m, Cliente cli) throws InterruptedException {
-		boolean adentro = false;
-		while(true) {
-			synchronized (this) {
-				if(mensajes.size() < max) {
-					mensajes.add(m);
-					adentro = true;
-					this.notifyAll();
-				}
-				else {
-					Thread.yield();
-				}
-			}
-			if(adentro) {
-				cli.wait();
-			}
+	private static Buffer buffer;
+	
+	private Buffer() {
+		mensajes = new ArrayList<Mensaje>();
+		clientesA = new ArrayList<Cliente>();
+		nMensajes = 0;
+	}	
+	
+	public static Buffer getBuffer() {
+		if(buffer == null) {
+			buffer = new Buffer();
 		}
+		return buffer;
 	}
+	public synchronized boolean meter(Mensaje m, Cliente cli) throws InterruptedException {
+		boolean adentro = false;
+		if(nMensajes>max)
+			adentro=false;
+		else
+		{
+			mensajes.add(m);
+			nMensajes++;
+			if(!clientesA.contains(cli))
+			{
+				clientesA.add(cli);
+			}
+			adentro=true;
+			notifyAll();
+			
+		}
+		return adentro;
+	}
+	
+	
+	
 	
 	public void retirar(Servidor ser){
 		while(true){
