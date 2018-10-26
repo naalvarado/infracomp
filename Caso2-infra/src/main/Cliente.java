8126532323c2c8afe0b835ebc0f88f9a39c62800
na.alvarado <na.alvarado@uniandes.edu.co>
2018-10-25 22:04:38 -0500

    ya intercambia sertificado y llaves

--- Caso2-infra/src/main/Cliente.java
+++ Caso2-infra/src/main/Cliente.java
@@ -1,39 +1,50 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.net.Socket;
 import java.security.*;
-import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-import javax.security.auth.x500.X500Principal;
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
-import org.bouncycastle.x509.*;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 public class Cliente {
 	
 	private static Socket s;
 	private static BufferedReader bReader;
 	private static PrintWriter pWriter;
+	private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
 	private static final String[] ALGORITMOS = {"AES","Blowfish","RSA","HMACMD5","HMACSHA1","HMACSHA256"};
 	private static KeyPair keypair;
-	private static PrivateKey privKey;
-	private static PublicKey pubKey;
+	private static X509Certificate serverCert;
+	private static SecretKey serverPublicKey;
 	
 	private static void crearllaves() throws Exception {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(1024);
         keypair = keyGen.generateKeyPair();
-        privKey = keypair.getPrivate();
-        pubKey = keypair.getPublic();
 	}
 	
 	public static void conectarseS(int puerto) throws Exception{
@@ -58,6 +69,7 @@ public class Cliente {
 		for(int i : pLista) {
 			mes = mes + ":" + ALGORITMOS[i-1];
 		}
+		System.out.println(mes);
 		pWriter.println(mes);
 		String re = bReader.readLine();
 		System.out.println("SERVIDOR: "+re);
@@ -77,26 +89,21 @@ public class Cliente {
 	
 	public static X509Certificate generarCertificado() throws Exception{
 		
+		Provider bcProvider = new BouncyCastleProvider();
+	    Security.addProvider(bcProvider);
+		
 		Calendar expiry = Calendar.getInstance();
         expiry.add(Calendar.DAY_OF_YEAR, 2);
         Date startDate = new Date(System.currentTimeMillis());
         Date endDate = expiry.getTime();
         BigInteger serialNum = BigInteger.valueOf(System.currentTimeMillis());
-        //X509Certificate caCert = (X509Certificate) pubKey;
+        X500Name dnName = new X500Name("cn=example");
         
-        X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
-        X500Principal              subjectName = new X500Principal("CN=Certificate");
-        certGen.setSerialNumber(serialNum);
-        //certGen.setIssuerDN(caCert.getSubjectX500Principal());
-        certGen.setNotBefore(startDate);
-        certGen.setNotAfter(endDate);
-        certGen.setSubjectDN(subjectName);
-        certGen.setPublicKey(pubKey);
-        // TODO no funciona porque no c que poner en SignatureAlgorithm
-        certGen.setSignatureAlgorithm("sha1RSA");
+        ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSA").build(keypair.getPrivate());
         
-        X509Certificate cert = certGen.generate(privKey, "BC");
-        return cert;
+        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(dnName, serialNum, startDate, endDate, dnName, keypair.getPublic());
+        
+        return new JcaX509CertificateConverter().setProvider(bcProvider).getCertificate(certBuilder.build(contentSigner));
 		
 	}
 	
@@ -107,6 +114,71 @@ public class Cliente {
 		pWriter.println(certificadoEnString);
 		String re = bReader.readLine();
 		System.out.println("SERVIDOR: "+re);
+	}
+	
+	public static void recivirCertSer() throws Exception {
+		String servercert = bReader.readLine();
+		System.out.println(servercert);
+		byte[] RAWcert = new byte[1000];
+		RAWcert = hexStringToByteArray(servercert);
+		System.out.println("copia el certificado el byte");
+		serverCert = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(RAWcert));
+		System.out.println("le da valor al serverCent");
+		pWriter.println("OK");
+	}
+	
+	public static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+	}
+	
+	public static void recivirkey() throws Exception {
+		String serverKeys = bReader.readLine();
+		System.out.println(serverKeys);
+		byte[] RAWserverKey = hexStringToByteArray(serverKeys);
+		Cipher cipher = Cipher.getInstance(ALGORITMOS[2]); 
+		cipher.init(Cipher.DECRYPT_MODE, keypair.getPrivate());
+		byte[] serverKey = cipher.doFinal(RAWserverKey);
+		serverPublicKey = new SecretKeySpec(serverKey,0,serverKey.length,ALGORITMOS[2]);
+	}
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+	
+	public static void sendKey() throws Exception{
+		byte[] key = serverPublicKey.getEncoded();
+		Cipher cipher = Cipher.getInstance(ALGORITMOS[2]);
+		cipher.init(Cipher.ENCRYPT_MODE, serverCert.getPublicKey());
+		byte[] encryptedKey = cipher.doFinal(key);
+		String keyS = bytesToHex(encryptedKey);
+		pWriter.println(keyS);
+		String re = bReader.readLine();
+		System.out.println("SERVIDOR: "+re);
+	}
+	
+	public static void consultaConHMAC(String numC) throws Exception{
+		Cipher cipher = Cipher.getInstance(ALGORITMOS[2]);
+		cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+		byte[] mess = cipher.doFinal(numC.getBytes());
+		
+	    Mac mac = Mac.getInstance(ALGORITMOS[2]);
+	    mac.init(serverPublicKey);
+	    byte[] macB = mac.doFinal(numC.getBytes());
+	    
+	    pWriter.println(DatatypeConverter.printHexBinary(mess));
+	    pWriter.println(DatatypeConverter.printHexBinary(macB));
 	}
 
 	public static void main(String[] args) {
@@ -150,11 +222,17 @@ public class Cliente {
 			enviarAlgoritmos(algor);
 			crearllaves();
 			enviarCertificado();
+			recivirCertSer();
+			recivirkey();
+			sendKey();
+			System.out.println("Introdusca el numero de cuanta: ");
+			String num = reader.nextLine();
+			consultaConHMAC(num);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		reader.close();
 	}
 
 }
